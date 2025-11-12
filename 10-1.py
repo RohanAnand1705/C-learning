3ba5abf3f7f297c9be154d2750830b1af38209cd
ROHAN ANAND <anandrohan1705@gmail.com>
2025-11-12 12:53:08 +0530

    add

---
+++ 10-1.py
@@ -0,0 +1,12 @@
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+
+sum = num1 + num2
+diff = num1 - num2
+mult = num1 * num2
+div = num1 / num2
+
+print("Addition:", sum)
+print("Subtraction:", diff)
+print("Multiplication:", mult)
+print("Division:", div)
